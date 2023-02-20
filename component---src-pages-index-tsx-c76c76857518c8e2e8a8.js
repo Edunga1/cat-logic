3fc3de41caacae3138bcdb821b27d2a5e7837c03
Edunga1 <Edunga1@users.noreply.github.com>
2023-02-20 16:37:06 +0000

    deploy: 014bda72d3ac221906b377e449ca0cf579891960

--- component---src-pages-index-tsx-c76c76857518c8e2e8a8.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcat_logic=self.webpackChunkcat_logic||[]).push([[691],{7200:function(e,t,n){n.r(t),n.d(t,{Head:function(){return c}});var a=n(7294);const o={color:"#232129",padding:96,fontFamily:"-apple-system, Roboto, sans-serif, serif"},l={marginTop:0,marginBottom:64,maxWidth:400};t.default=e=>(console.log(e),a.createElement("main",{style:o},a.createElement("h1",{style:l},"WORK IN PROGRESS")));const c=()=>a.createElement("title",null,"Home Page")}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-c76c76857518c8e2e8a8.js.map
