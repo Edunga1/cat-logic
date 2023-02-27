f773aadaadef7b00f0c275407790eb6ca22d4fb1
edunga1 <goonr21@gmail.com>
2023-02-27 14:28:09 +0000

    Update sites
    add styled-components to use improved way of styling
    update wiki page

--- sites/src/pages/wiki/{markdownRemark.fields__slug}.tsx
+++ sites/src/pages/wiki/{markdownRemark.fields__slug}.tsx
@@ -1,14 +1,38 @@
 import * as React from "react"
 import { graphql, PageProps } from "gatsby"
+import styled from "styled-components"
+
+const ContainerStyles = {
+  display: "grid",
+  justifyItems: "center",
+}
+
+const ContentStyles = {
+  display: "grid",
+  gridTemplateColumns: "minmax(300px, 1fr) minmax(400px, 1000px)",
+  width: "fit-content",
+}
+
+const Toc = styled.div`
+  margin: 4rem 2rem 0 0;
+  & ul {
+    list-style: none;
+    & p {
+      margin: 0;
+    }
+  }
+`
 
 export default function BlogPostTemplate(
   { data }: PageProps<Queries.WikiDetailQuery>,
 ) {
   const { tableOfContents, html } = data.markdownRemark ?? {}
   return (
-    <div>
-      {tableOfContents && <div dangerouslySetInnerHTML={{ __html: tableOfContents }} />}
-      {html && <div dangerouslySetInnerHTML={{ __html: html }} />}
+    <div style={ContainerStyles}>
+      <div style={ContentStyles}>
+        {tableOfContents && <Toc dangerouslySetInnerHTML={{ __html: tableOfContents }} />}
+        {html && <div dangerouslySetInnerHTML={{ __html: html }} />}
+      </div>
     </div>
   )
 }
