bc2b0f671ba75092c1961b2a773d4402dadeede4
edunga1 <goonr21@gmail.com>
2023-03-07 12:37:45 +0000

    Update sites
    apply media query to wiki page

--- sites/src/pages/wiki/{markdownRemark.fields__slug}.tsx
+++ sites/src/pages/wiki/{markdownRemark.fields__slug}.tsx
@@ -8,11 +8,17 @@ const ContainerStyles = {
   justifyItems: "center",
 }
 
-const ContentStyles = {
-  display: "grid",
-  gridTemplateColumns: "minmax(300px, 1fr) minmax(400px, 1000px)",
-  width: "fit-content",
-}
+const Content = styled.div`
+  display: grid;
+  grid-template-columns: minmax(300px, 1fr) minmax(400px, 1000px);
+  width: fit-content;
+  @media (max-width: 1000px) {
+    grid-template-columns: 1fr;
+    & > div:nth-child(1) {
+      display: none;
+    }
+  }
+`
 
 const Toc = styled.div`
   margin: 4rem 2rem 0 0;
@@ -31,10 +37,10 @@ export default function BlogPostTemplate(
   const rhtml = html && replaceWikiLinks(html)
   return (
     <div style={ContainerStyles}>
-      <div style={ContentStyles}>
+      <Content>
         {tableOfContents && <Toc dangerouslySetInnerHTML={{ __html: tableOfContents }} />}
         {rhtml && <div dangerouslySetInnerHTML={{ __html: rhtml }} />}
-      </div>
+      </Content>
     </div>
   )
 }
