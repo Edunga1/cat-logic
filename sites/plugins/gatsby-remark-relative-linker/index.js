122f18f457b197e99fdf47ecd9847b5190be3ee2
edunga1 <goonr21@gmail.com>
2023-11-07 14:54:25 +0000

    Update sites
    refactor gastby-remark-relative-linker local plugin

--- sites/plugins/gatsby-remark-relative-linker/index.js
+++ sites/plugins/gatsby-remark-relative-linker/index.js
@@ -1,9 +1,14 @@
 const visit = require("unist-util-visit")
 
+/**
+ * 다른 Markdown 파일에 대한 링크를 상대 경로로 변경
+ *
+ * `gatsby-transformer-remark`의 플러그인으로써 마크다운 변환 과정에서 처리됨
+ * ref. https://stackoverflow.com/q/48553146/6587078
+ */
 module.exports = function({ markdownAST }) {
   visit(markdownAST, "link", node => {
     if (isRelativeLink(node.url)) {
-      const old = node.url
       node.url = node.url.replace(/.*\/(.+)\.md(#.*)?$/, (_, base, hash) => {
         return `../${base}${hash || ""}`
       })
