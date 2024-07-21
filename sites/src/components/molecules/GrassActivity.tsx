03b2a85749b98ba99641405d36eceabbfb803dbd
edunga1 <goonr21@gmail.com>
2024-07-21 10:05:08 +0000

    Update sites
    added activity label

--- sites/src/components/molecules/GrassActivity.tsx
+++ sites/src/components/molecules/GrassActivity.tsx
@@ -4,10 +4,9 @@ import styled from "styled-components"
 // TODO: MUST REFACTOR THIS COMPONENT
 
 const Container = styled.div`
-  height: 1rem;
+  height: 3rem;
   position: relative;
   display: flex;
-  align-items: center;
   // TODO: this might be tricky. children are out of the container
   margin-right: .5rem;
 `
@@ -24,6 +23,21 @@ const Point = styled.div<
   left: ${props => props.left.toFixed(2)}%;
   background-color: ${props => props.fixed ? "#eee" : "#c0debf"};
   z-index: ${props => props.fixed ? 0 : 1};
+
+  > span {
+    font-size: .5rem;
+    top: 1rem;
+    white-space: nowrap;
+    transform: rotate(-15deg);
+    position: inherit;
+    cursor: default;
+    user-select: none;
+  }
+
+  &:hover {
+    background-color: #67c767;
+    z-index: 2;
+  }
 `
 
 export default function GrassActivity(
@@ -48,6 +62,7 @@ export default function GrassActivity(
               left={point.rate * 100}
               fixed={point.isFixed}
             >
+              <span>{point.label}</span>
             </Point>
           )
         })
@@ -76,31 +91,35 @@ function minusDays(date: Date, days: number) {
   return new Date(date.setDate(date.getDate() - days))
 }
 
-function calculatePoints(steps: Date[], dates: Date[]): Point[] {
+function calculatePoints(steps: Date[], dates: Date[]): PointData[] {
   const range = steps[steps.length - 1].getTime() - steps[0].getTime()
   const points = steps.map(x => (
-    {
-      date: x,
-      rate: (x.getTime() - steps[0].getTime()) / range,
-      isFixed: true,
-    }
+    new PointData(
+      x,
+      (x.getTime() - steps[0].getTime()) / range,
+      true,
+      x.toLocaleDateString(),
+    )
   ))
   return dates
     .filter(x => x >= steps[0] && x <= steps[steps.length - 1])
     .sort((a, b) => a.getTime() - b.getTime())
     .reduce((acc, date) => {
-      const p = {
+      const p = new PointData(
         date,
-        rate: (date.getTime() - steps[0].getTime()) / range,
-        isFixed: false,
-      }
+        (date.getTime() - steps[0].getTime()) / range,
+        false,
+      )
       acc.splice(acc.length - 1, 0, p)
       return acc
     }, points)
 }
 
-type Point = {
-  date: Date
-  rate: number
-  isFixed: boolean
+class PointData {
+  constructor(
+    public date: Date,
+    public rate: number,
+    public isFixed: boolean,
+    public label?: string,
+  ) { }
 }
