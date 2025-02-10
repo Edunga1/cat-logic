f8edc38f0e270d0da14320b0b49fd9eeed95e1d9
edunga1 <goonr21@gmail.com>
2025-02-10 14:15:26 +0900

    Refactor find-missing-front-matter.sh

--- scripts/find-missing-front-matter.sh
+++ scripts/find-missing-front-matter.sh
@@ -13,9 +13,9 @@ WORD='created:'
 find "$DIR" -type f -name "*.md" | while read file; do
   # 파일의 front matter 부분에서 문자열로 시작하는 줄이 있는지 확인
   if ! awk -v word="$WORD" '
-    /^---$/ {f=1; next} 
-    f && /^---$/ {f=0} 
-    f && $0 ~ word {found=1} 
+    /^---$/ {f=1; next}
+    f && /^---$/ {f=0}
+    f && $0 ~ word {found=1}
     END {exit !found}
   ' "$file"; then
     echo "No '$WORD' found in front matter of: $file"
