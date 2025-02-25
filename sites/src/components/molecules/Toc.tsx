f8f4b66543cbb90419f2de6d6a286332e18bc70f
edunga1 <goonr21@gmail.com>
2025-02-25 22:24:52 +0900

    Update sites
    added table of contents header

--- sites/src/components/molecules/Toc.tsx
+++ sites/src/components/molecules/Toc.tsx
@@ -2,9 +2,15 @@ import * as React from "react"
 import styled from "styled-components"
 import theme from "../../constants/theme"
 
+const Header = styled.h3`
+  margin: 0;
+  padding: 1rem 0 .5rem 1rem;
+  font-size: 1rem;
+`
+
 const Container = styled.div`
   box-sizing: border-box;
-  padding: 1rem 1rem 1rem 0;
+  padding: 0 1rem 1rem 0;
 
   ul {
     list-style: none;
@@ -37,6 +43,9 @@ export default function Toc(
   },
 ) {
   return (
-    <Container className={className} dangerouslySetInnerHTML={{ __html: contents }} />
+    <div className={className} >
+      <Header>Table of Contents</Header>
+      <Container dangerouslySetInnerHTML={{ __html: contents }} />
+    </div>
   )
 }
