0f35935f65940e5dc387ce52b0f8d4ecf6547ea4
edunga1 <goonr21@gmail.com>
2025-03-06 23:11:46 +0900

    Update embeddings
    added a script to replace the similarity result file in the site

---
+++ embeddings/renew-similarity.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -d "target" ]; then
+  rm -rf target
+fi
+
+cp -r ../docs/wiki target
+
+docker run --rm -t -i -v `pwd`:/app -e AZURE_OPENAI_API_KEY=$AZURE_OPENAI_API_KEY embeddings uv run src/get_embeddings.py "target/**/*.md"
+
+docker run --rm -t -i -v `pwd`:/app embeddings uv run src/cosine_similarity.py output_embeddings.csv
+
+cp similarity_result.json ../sites/src/related-docs/similarity-result.json
