68e6e011b6600bc1e7e0e2cc01424bc7f0848698
edunga1 <goonr21@gmail.com>
2025-04-07 23:40:24 +0900

    Update embeddings
    updated spacing 4 spaces to 2 spaces
    added loading from SQLite in progress

--- embeddings/src/cosine_similarity.py
+++ embeddings/src/cosine_similarity.py
@@ -4,23 +4,23 @@ import numpy as np
 
 
 def cosine_similarity(a, b):
-    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+  return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
 def save_df_to_json(df, filename):
-    df.to_json(filename, orient='records')
+  df.to_json(filename, orient='records')
 
 
 if __name__ == '__main__':
-    """
-    $ python src/cosine_similarity.py data.csv
-    """
-    df = pd.read_csv(sys.argv[1])
-    df = df[['filename', 'embedding']]
-    df['embedding'] = df['embedding'].apply(lambda x: eval(x))
-    df = df.merge(df, how='cross')
-    df['similarity'] = df.apply(lambda x: cosine_similarity(x['embedding_x'], x['embedding_y']), axis=1)
-    df_result = df[['filename_x', 'filename_y', 'similarity']]
-    print(df_result)
-    save_df_to_json(df_result, 'similarity-result.json')
-    print('Saved to similarity-result.json')
+  """
+  $ python src/cosine_similarity.py data.csv
+  """
+  df = pd.read_csv(sys.argv[1])
+  df = df[['filename', 'embedding']]
+  df['embedding'] = df['embedding'].apply(lambda x: eval(x))
+  df = df.merge(df, how='cross')
+  df['similarity'] = df.apply(lambda x: cosine_similarity(x['embedding_x'], x['embedding_y']), axis=1)
+  df_result = df[['filename_x', 'filename_y', 'similarity']]
+  print(df_result)
+  save_df_to_json(df_result, 'similarity-result.json')
+  print('Saved to similarity-result.json')
--- embeddings/src/get_embeddings.py
+++ embeddings/src/get_embeddings.py
@@ -23,79 +23,88 @@ max_tokens = 8000  # the maximum for text-embedding-ada-002 is 8191
 
 
 def read_docs(directory):
-    files = pathlib.Path(directory).rglob('*.md')
-    df = pd.DataFrame([], columns=['filename', 'text'])
+  files = pathlib.Path(directory).rglob('*.md')
+  df = pd.DataFrame([], columns=['filename', 'text'])
 
-    for file in files:
-        with open(file, 'r') as f:
-            text = f.read()
-            checksum = hashlib.sha1(text.encode()).hexdigest()
-            df_file = pd.DataFrame(
-                [[str(file.relative_to(directory)), text, checksum]],
-                columns=['filename', 'text', 'checksum'],
-            )
-            df = pd.concat([df, df_file])
-    return df
+  for file in files:
+    with open(file, 'r') as f:
+      text = f.read()
+      checksum = hashlib.sha1(text.encode()).hexdigest()
+      df_file = pd.DataFrame(
+        [[str(file.relative_to(directory)), text, checksum]],
+        columns=['filename', 'text', 'checksum'],
+      )
+      df = pd.concat([df, df_file])
+  return df
 
 
 def update_token(df):
-    df = df.copy()
-    encoding = tiktoken.get_encoding(embedding_encoding)
-    df['tokens'] = df['text'].apply(lambda x: encoding.encode(x))
-    df['n_tokens'] = df['tokens'].apply(lambda x: len(x))
-    df['truncated_text'] = df['tokens'].apply(
-        lambda x: encoding.decode(x[:max_tokens]))
-    df['truncated_n_tokens'] = df['truncated_text'].apply(
-        lambda x: len(encoding.encode(x)))
-    # convert tokens to string to persist
-    df['tokens'] = df['tokens'].apply(lambda x: str(x))
-    return df
+  df = df.copy()
+  encoding = tiktoken.get_encoding(embedding_encoding)
+  df['tokens'] = df['text'].apply(lambda x: encoding.encode(x))
+  df['n_tokens'] = df['tokens'].apply(lambda x: len(x))
+  df['truncated_text'] = df['tokens'].apply(
+    lambda x: encoding.decode(x[:max_tokens]))
+  df['truncated_n_tokens'] = df['truncated_text'].apply(
+    lambda x: len(encoding.encode(x)))
+  # convert tokens to string to persist
+  df['tokens'] = df['tokens'].apply(lambda x: str(x))
+  return df
 
 
 def get_embeddings(df):
-    df = df.copy()
-    embedder = AzureOpenAIEmbedder(
-        azure_endpoint=embedding_base_url,
-        model=embedding_model,
-        api_key=azure_api_key,
-    )
+  df = df.copy()
+  embedder = AzureOpenAIEmbedder(
+    azure_endpoint=embedding_base_url,
+    model=embedding_model,
+    api_key=azure_api_key,
+  )
 
-    def process(x):
-        try:
-            embedding = embedder.get_embedding(x)
-            sleep(5)  # to avoid rate limiting. based on GitHub Models Free Tier
-        except Exception as e:
-            print(e)
-            embedding = None
-        return embedding
-    df['embedding'] = df['truncated_text'].apply(process)
-    return df
+  def process(x):
+    try:
+      embedding = embedder.get_embedding(x)
+      sleep(5)  # to avoid rate limiting. based on GitHub Models Free Tier
+    except Exception as e:
+      print(e)
+      embedding = None
+    return embedding
+  df['embedding'] = df['truncated_text'].apply(process)
+  return df
 
 
 def save_sqlite(df, filename):
-    conn = sqlite3.connect(filename)
-    df.to_sql('docs', conn, if_exists='replace', index=False)
+  conn = sqlite3.connect(filename)
+  df.to_sql('docs', conn, if_exists='replace', index=False)
+
+
+def load_embeddings(filename):
+  conn = sqlite3.connect(filename)
+  df = pd.read_sql('SELECT * FROM docs', conn)
+  return df
 
 
 def persist_embeddings(df, filename):
-    df.to_csv(f'{filename}.csv')
+  df.to_csv(f'{filename}.csv')
 
-    # TODO: Raise an error on objec types
-    df['embedding'] = df['embedding'].apply(lambda x: json.dumps(x) if x else None)
-    save_sqlite(df, f'{filename}.db')
+  # TODO: Raise an error on objec types
+  df['embedding'] = df['embedding'].apply(lambda x: json.dumps(x) if x else None)
+  save_sqlite(df, f'{filename}.db')
 
 
 if __name__ == '__main__':
-    """
-    $ python src/get_embeddings.py ../docs/wiki
-    """
-    df = read_docs(sys.argv[1])
-    df = update_token(df)
+  """
+  $ python src/get_embeddings.py ../docs/wiki
+  """
+  # TODO: Implement loading from SQLite
+  # db_file = sys.argv[2] if len(sys.argv) > 2 else 'output_embeddings.db'
+  # df = load_embeddings(db_file)
+  df = read_docs(sys.argv[1])
+  df = update_token(df)
 
-    # this calls the OpenAI API. May be charged.
-    df = get_embeddings(df)
+  # this calls the OpenAI API. May be charged.
+  df = get_embeddings(df)
 
-    persist_embeddings(df, 'output_embeddings')
+  persist_embeddings(df, 'output_embeddings')
 
-    print(df)
-    print(df.dtypes)
+  print(df)
+  print(df.dtypes)
