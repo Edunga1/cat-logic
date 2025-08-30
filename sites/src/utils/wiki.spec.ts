fd8c6b1b45e0a72fc26d8d0b1b42b97ce1415aeb
edunga1 <goonr21@gmail.com>
2025-08-30 14:14:29 +0900

    Update sites
    fixed failing tests

--- sites/src/utils/wiki.spec.ts
+++ sites/src/utils/wiki.spec.ts
@@ -5,13 +5,13 @@ import { createWikiLink } from "./wiki"
 describe("createWikiLink method", () => {
   describe("when the slug starts with a slash", () => {
     it("returns a link with the slug", () => {
-      assert.strictEqual(createWikiLink("/home"), "./wiki/home")
+      assert.strictEqual(createWikiLink("/home"), "/wiki/home")
     })
   })
 
   describe("when the slug does not start with a slash", () => {
     it("returns a link with the slug", () => {
-      assert.strictEqual(createWikiLink("home"), "./wiki/home")
+      assert.strictEqual(createWikiLink("home"), "/wiki/home")
     })
   })
 })
